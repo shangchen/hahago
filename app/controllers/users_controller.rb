be5b0987e369fbfeb064d3733a9673f093488c67
shangchen <shangchen.chang@gmail.com>
2017-01-14 22:17:01 +0800

    hahago 0.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-		  log_in @user
+		  #log_in @user
 		  flash[:success] = "Welcome to the HaHaGo!"
 		  redirect_to @user
 		else
