0c8ab4d4464eaae34bcef8d15af2276bf0fc1815
shangchen <shangchen.chang@gmail.com>
2017-01-15 21:23:53 +0800

    add active user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,13 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 		  log_in @user
-		  # flash[:success] = "Welcome to the HaHaGo!"
-		  # redirect_to root_url
-		  UserMailer.account_activation(@user).deliver_now
+		   flash[:success] = "Welcome to the HaHaGo!"
+		   redirect_to root_url
+		  #UserMailer.account_activation(@user).deliver_now
 	      # @user.send_activation_email
-	      flash[:info] = "Please check your email to activate your account."
-	      redirect_to root_url		  
+	      # flash[:info] = "Welcome to the HaHaGo!
+	      # Please check your email to activate your account."
+	      # redirect_to root_url		  
 		else
 		  render 'new'
 		end
