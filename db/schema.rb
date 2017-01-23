02dddc974e08450b2db56b66fa1bbce651513bfe
shangchen <shangchen.chang@gmail.com>
2017-01-23 16:40:05 +0800

    adjust ga to head

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170115120901) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
-    t.boolean  "activated"
+    t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
