e6f6535258897d7bdc65d392a0c32889c2e6f2b3
shangchen <shangchen.chang@gmail.com>
2017-02-18 15:52:29 +0800

    modify smpt to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.delivery_method = :smtp
   host = 'hahago.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
